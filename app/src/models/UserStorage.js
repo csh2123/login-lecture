381ceef3a55d4dd4ffaffe988391a2d9baf3763e
csh2123 <dltmfqhek2@gmail.com>
2024-08-27 21:37:01 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,19 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
     //#은 public -> private 
+    /* 26강에서 users.json 으로 db이동
     static #users = {
         id: ["csh2123", "나개발", "김팀장"],
         psword: ["1234", "1234", "123456"],
         name: ["홍길동","나철수","김철수"]
     };
+    */
 
     static getUsers(...fields){
-        const users = this.#users;
+        //const users = this.#users;      
         const newUsers = fields.reduce((newUsers, field) => {            
             if(users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -20,18 +24,41 @@ class UserStorage {
     }
 
     static getUserInfo(id){
-        const users = this.#users;
+        return fs
+          .readFile("./src/databases/users.json")
+          .then((data) => {
+            /*
+            const users = JSON.parse(data);
+            const idx = users.id.indexOf(id);
+            const usersKeys = Object.keys(users);    // =>[id, psword, name]
+            const userInfo = usersKeys.reduce((newUser, info) => {
+                newUser[info] = users[info][idx];
+                return newUser;
+            }, {});
+            
+            return userInfo;
+            */
+           return this.#getUserInfo(data, id);
+          })
+          .catch(console.error);
+    }
+ 
+    static #getUserInfo(data, id){
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
-        const userInfo = Object.keys(users).reduce((newUser, info) => {
+        const usersKeys = Object.keys(users);    // =>[id, psword, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
             newUser[info] = users[info][idx];
             return newUser;
         }, {});
-
+        
         return userInfo;
     }
+    
+
 
     static save(userInfo) {
-        const users = this.#users;
+        //const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
